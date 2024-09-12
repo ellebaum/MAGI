3e68767f9a1b72520a405cba099812604e005c1e
Elle Baumgartel <estelle.baumgartel@gmail.com>
2024-09-12 16:12:34 -0400

    Fix calculation state bug
    Fixes an issue where the old answer state would be "stuck" when the user
    asks a new question. This change will force the status and wise men
    backgrounds to always be in the "info" state during a calculation

--- components/response.js
+++ components/response.js
@@ -1,59 +1,54 @@
-import React from 'react';
+import React from "react";
 const $ = React.createElement;
 
 function getStatusText(status) {
-    if (status === 'info')
-        return '情 報';
+  if (status === "info") return "情 報";
 
-    if (status === 'yes')
-        return '合 意';
+  if (status === "yes") return "合 意";
 
-    if (status === 'no')
-        return '拒 絶';
+  if (status === "no") return "拒 絶";
 
-    if (status === 'conditional')
-        return '状 態';
+  if (status === "conditional") return "状 態";
 
-    if (status === 'error')
-        return '誤 差'
+  if (status === "error") return "誤 差";
 
-    throw new Error('Invalid status: ' + status);
+  throw new Error("Invalid status: " + status);
 }
 
 function getStatusColor(status) {
-    if (status === 'info')
-        return '#3caee0';
+  if (status === "info") return "#3caee0";
 
-    if (status === 'yes')
-        return '#52e691';
+  if (status === "yes") return "#52e691";
 
-    if (status === 'no')
-        return '#a41413';
+  if (status === "no") return "#a41413";
 
-    if (status === 'conditional')
-        return '#ff8d00';
+  if (status === "conditional") return "#ff8d00";
 
-    if (status === 'error')
-        return 'gray';
+  if (status === "error") return "gray";
 
-    throw new Error('Invalid status: ' + status);
+  throw new Error("Invalid status: " + status);
 }
 
 export default function Response({ status, question_id, answer_id }) {
-    const text = getStatusText(status);
-    const color = getStatusColor(status);
+  let text = getStatusText(status);
+  let color = getStatusColor(status);
+  let className = "response";
 
-    let className = 'response';
-    if (question_id !== answer_id)
-        className += ' flicker';
+  if (question_id !== answer_id) {
+    className += " flicker";
+    text = getStatusText("info");
+    color = getStatusColor("info");
+  }
 
-    return $('div', { className, style: { color: color, borderColor: color } },
-        $('div', { className: 'inner' }, text)
-    );
+  return $(
+    "div",
+    { className, style: { color: color, borderColor: color } },
+    $("div", { className: "inner" }, text)
+  );
 }
 
 Response.defaultProps = {
-    status: 'info',
-    question_id: 0,
-    answer_id: 0
+  status: "info",
+  question_id: 0,
+  answer_id: 0,
 };
